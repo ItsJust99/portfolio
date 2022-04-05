2f1995f845d909b0f67d73d23c1b1b5e745cb630
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2022-04-05 20:11:51 +0200

    loadscreen + js page

---
+++ script.js
@@ -0,0 +1,22 @@
+// class "loader hidden"
+window.addEventListener("load", function () { 
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; 
+});
+
+//project slider
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+}); 
