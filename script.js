bdc8b771ae1f6c3b21b04d67563aaa10734bd0b3
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2022-08-29 12:41:42 +0200

    update2

--- script.js
+++ script.js
@@ -21,20 +21,8 @@ let mixerPortfolio = mixitup('.work__container', {
       duration: 300
   }
 });
-//link active work
-const linkWork = document.querySelectorAll('.work__item')
-function activeWork(){
-  linkWork.forEach(l=> l.classList.remove('active-work'))
-  this.classList.add('active-work')
-}
-linkWork.forEach(l=>addEventListener("click", activeWork))
-//popup projecten
-document.addEventListener("click", (e) =>{
-  if(e.target.classList.contains("work__button")){
-    togglePortfolioPopup();
-  }
-})
 
-function togglePortfolioPopup(){
-  document.querySelector(".portfolio__popup").classList.toggle("open");
-}
+
+
+
+
