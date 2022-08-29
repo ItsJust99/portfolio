fa97a5e94e47ae472e91c64e94b7a1efd6ae3731
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2022-08-29 10:57:52 +0200

    verandert update

--- script.js
+++ script.js
@@ -2,23 +2,6 @@
 window.addEventListener("load", function () { 
     const loader = document.querySelector(".loader");
     loader.className += " hidden"; 
-});
-
-//project slider
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
 }); 
 
 //progressbar
@@ -29,38 +12,29 @@ window.onscroll = function(){
     progress.style.height = progressHeight + "%";
 }
 
-//Contact popup
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+//mixitup projecten
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
   }
+});
+//link active work
+const linkWork = document.querySelectorAll('.work__item')
+function activeWork(){
+  linkWork.forEach(l=> l.classList.remove('active-work'))
+  this.classList.add('active-work')
 }
+linkWork.forEach(l=>addEventListener("click", activeWork))
+//popup projecten
+document.addEventListener("click", (e) =>{
+  if(e.target.classList.contains("work__button")){
+    togglePortfolioPopup();
+  }
+})
 
-//popup project2
-function togglePopup(){
-  document.getElementById("popup1").classList.toggle("active");
-}
-//popup project4
-function togglePopup4(){
-  document.getElementById("popup4").classList.toggle("active");
+function togglePortfolioPopup(){
+  document.querySelector(".portfolio__popup").classList.toggle("open");
 }
