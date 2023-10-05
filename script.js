9fe0cd597e34a9683bf10267522d4adaa00fc71e
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-10-05 13:34:47 +0200

    cookies poging 2

--- script.js
+++ script.js
@@ -22,17 +22,32 @@ let mixerPortfolio = mixitup('.work__container', {
   }
 });
 
-//cookie popup
-let popUp = document.getElementById("cookiePopup");
-document.getElementById("acceptCookie");
-addEventListener("click", () => {
-    let d = new Date();
-    d.setMinutes(2 + d.getMinutes());
-    document.cookie = "myCookieName=thisIsMyCookie";
-    expires =  + d + ";";
-    popUp.classList.add("hide");
-    popUp.classList.remove("show");
+// //cookie popup
+document.getElementById("accept-cookies").addEventListener("click", function () {
+    // Set a cookie to remember user's choice
+    document.cookie = "cookiesAccepted=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+
+    // Remove the cookie banner
+    document.querySelector(".cookie-banner").style.display = "none";
 });
+
+// Check if the user has already accepted cookies
+if (document.cookie.indexOf("cookiesAccepted=true") !== -1) {
+    document.querySelector(".cookie-banner").style.display = "none";
+}
+
+
+
+// let popUp = document.getElementById("cookiePopup");
+// document.getElementById("acceptCookie");
+// addEventListener("click", () => {
+//     let d = new Date();
+//     d.setMinutes(2 + d.getMinutes());
+//     document.cookie = "myCookieName=thisIsMyCookie";
+//     expires =  + d + ";";
+//     popUp.classList.add("hide");
+//     popUp.classList.remove("show");
+// });
 
 
 
